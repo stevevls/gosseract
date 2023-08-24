b3cf1cd41a0d54072f9413eec2808c5247f5f507
Steve van Loben Sels <svanlobensels@rippling.com>
2023-08-24 11:20:06 -0700

    Use pkg-config on mac for getting LDFLAGS

--- preprocessflags_darwin.go
+++ preprocessflags_darwin.go
@@ -3,5 +3,5 @@ package gosseract
 // #cgo CXXFLAGS: -std=c++0x
 // #cgo CPPFLAGS: -I/usr/local/include
 // #cgo CPPFLAGS: -Wno-unused-result
-// #cgo !darwin LDFLAGS: -L/usr/local/lib -lleptonica -ltesseract
+// #cgo pkg-config: lept tesseract
 import "C"
